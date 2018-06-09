7ff1b4325c7976b9a054a99cbfe25e40dc8b9dbf
am4nto <luigii.lui@gmail.com>
2018-06-09 14:42:58 +0200

    21.3v2

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -6,26 +6,25 @@ import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
-
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -87,14 +86,14 @@ public class TaskControllerTest {
     }
 
     @Test
-    public void testDeleteTask(String s) throws Exception {
+    public void testDeleteTask() throws Exception {
         //Given
 
         //When & Then
         mockMvc.perform(MockMvcRequestBuilders
                 //.delete("/v1/task/deleteTask/{id}", "1")
                 .delete("/v1/task/deleteTask/")
-                .param("id", "1")
+                .param("taskId","1")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
@@ -102,16 +101,34 @@ public class TaskControllerTest {
 
     @Test
     public void testUpdateTask() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.put("/v1/task/updateTask/{\n" +
-                "\t\"id\": 1,\n" +
-                "\t\"title\": \"test2\",\n" +
-                "\t\"content\": \"testing\"\n" +
-                "}")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        //Given
+        String createTaskDtoInJson = "{ \"id\": \"" + "1" + "\", " +
+                "\"title\":\"" + "test" + "\"," +
+                "\"content\":\"" + "content" + "\"}}";
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/v1/task/updateTask/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content (createTaskDtoInJson);
+
+        //When & Then
+        this.mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status()
+                        .isOk());
     }
 
     @Test
-    public void createTask() {
+    public void testCreateTask() throws Exception {
+
+        //Given
+        String createTaskDtoInJson = "{ \"id\": \"" + "1" + "\", " +
+                "\"title\":\"" + "test" + "\"," +
+                "\"content\":\"" + "content" + "\"}}";
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/v1/task/createTask/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content (createTaskDtoInJson);
+
+        //When & Then
+        this.mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status()
+                        .isOk());
     }
 }
